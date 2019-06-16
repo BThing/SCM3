c88690a40d1372fdba230fc2b6c6616e64f98c4e
phuong1234567 <51868077+phuong1234567@users.noreply.github.com>
2019-06-16 08:23:13 +0700

    Add files via upload

---
+++ frm_HinhTron.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CCPTTKHT
+{
+    public partial class frm_HinhTron : Form
+    {
+        public frm_HinhTron()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_DienTich_Click(object sender, EventArgs e)
+        {
+            double pi = Math.PI;
+            double Dientich = 0;
+            double bankinh = double.Parse(txt_BanKinh.Text);
+            Dientich = bankinh * pi;
+            txt_KetQua.Text = Dientich.ToString();
+        }
+    }
+}
