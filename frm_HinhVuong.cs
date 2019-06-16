92c733884530bd4dc01b0aa127e4ea2d66d1a6d7
Hung477 <51868105+Hung477@users.noreply.github.com>
2019-06-16 08:25:15 +0700

    Add files via upload

---
+++ frm_HinhVuong.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CCPTTKHT
+{
+    public partial class frm_HinhVuong : Form
+    {
+        public frm_HinhVuong()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_HinhVuong_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Tinh_Click(object sender, EventArgs e)
+        {
+            int DienTich = 0;
+            int canh = int.Parse(txt_Canh.Text);
+            DienTich = canh * canh;
+            txt_KetQua.Text = DienTich.ToString();
+        }
+    }
+}
