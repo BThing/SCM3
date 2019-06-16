8f8787a68a54b6d6c99092a0256694e46144afc6
vovanvinh181119977 <51841237+vovanvinh181119977@users.noreply.github.com>
2019-06-16 08:15:16 +0700

    Add files via upload

---
+++ frm_HinhChuNhat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CCPTTKHT
+{
+    public partial class frm_HinhChuNhat : Form
+    {
+        public frm_HinhChuNhat()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_HinhChuNhat_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Tinh_Click(object sender, EventArgs e)
+        {
+            int DienTich = 0;
+            int chieudai = int.Parse(txt_ChieuDai.Text);
+            int chieurong = int.Parse(txt_ChieuRong.Text);
+            DienTich = chieudai * chieurong;
+            txt_KetQua.Text = DienTich.ToString();
+        }
+    }
+}
